d5e62a06be2b580a64d73f1fb77e6117348f5bdc
ErmamatovIslombek <islombekermamatov2003@gmail.com>
2022-10-08 20:56:59 +0500

    imtihon commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// let ism = ["Keep", "Remove", "Keep", "Remove", "Keep", "Remove"];
+
+// console.log();
+
+
+// function second(arr) {
+//     let sum = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         if(i % 2 === 0 ) {
+//             sum.push(arr[i]);
+//         }     
+//     }
+//     return sum;
+// }
+
+// console.log(second(ism));
+
+
+// let ism = "salim aliyev";
+
+
+// function res(ism) {
+//     let res1 = ism.split(" ");
+//     let sum = "";
+//     for(let i = 0; i < res1.length; i++) {
+//         sum += res1[1][0].toUpperCase() + ".";
+//     }
+//     return sum;   
+// }
+
+// console.log(res(ism));
+
+
